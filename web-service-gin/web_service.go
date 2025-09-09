4e660a410b4a591b6cdc7b5cd994a5383fe4a774
brenobmoreira <brenobmoreira20@gmail.com>
2025-09-09 19:13:16 -0300

    get album by id

--- web-service-gin/web_service.go
+++ web-service-gin/web_service.go
@@ -23,6 +23,19 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -37,6 +50,7 @@ func postAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
