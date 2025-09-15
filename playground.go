870c2339e427a30b696f0bc2bf56ec6c58de1e10
brenobmoreira <brenobmoreira20@gmail.com>
2025-09-15 20:18:27 -0300

    solving problems

--- playground.go
+++ playground.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/brenobmoreira/breno-playground/http"
+	"github.com/brenobmoreira/breno-playground/api"
 )
 
 func main() {
 
 	var cep string
+
 	fmt.Println("Digite seu CEP: ")
 	fmt.Scanln(&cep)
-	cepBody, err = http.Cep(cep)
+	cepBody, err := api.Cep(cep)
 
 	if err != nil {
 		panic(err)
