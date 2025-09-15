677aaec6c62e2a3fccc079385b959a4d667a6e87
brenobmoreira <brenobmoreira20@gmail.com>
2025-09-15 20:53:46 -0300

    WIP - downloanding bases of datasus in ftp

---
+++ api/ftp/download.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"github.com/jlaffaye/ftp"
+)
+
+func main() {
+
+	type info struct {
+		UF  string `json:"uf"`
+		Ano string `json:"ano"`
+		Mes string `json:"mes"`
+	}
+
+	var infos = []info{
+		{UF: "SC", Ano: "25", Mes: "05"},
+		{UF: "SC", Ano: "25", Mes: "04"},
+		{UF: "SC", Ano: "25", Mes: "03"},
+	}
+
+	for i := range infos {
+		fmt.Println(infos[i])
+	}
+
+	resp, err := ftp.Dial("ftp.datasus.gov.br:21", ftp.DialWithTimeout(5*time.Second))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = resp.Login("anonymous", "anonymous")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	read, err := resp.Retr("/dissemin/publicos/CNES/200508_/Dados/EQ/STSC2404.dbc")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer read.Close()
+
+	buf, err := io.ReadAll(read)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buf))
+
+	if err := resp.Quit(); err != nil {
+		log.Fatal(err)
+	}
+}
