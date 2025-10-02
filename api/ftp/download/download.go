269efc3fa31bf4ed80dac5870803a1e7ae4e7de0
brenobmoreira <brenobmoreira20@gmail.com>
2025-10-01 21:31:10 -0300

    improving download file

--- api/ftp/download/download.go
+++ api/ftp/download/download.go
@@ -10,14 +10,13 @@ import (
 	"github.com/jlaffaye/ftp"
 )
 
+type info struct {
+	UF  string `json:"uf"`
+	Ano string `json:"ano"`
+	Mes string `json:"mes"`
+}
+
 func main() {
-
-	type info struct {
-		UF  string `json:"uf"`
-		Ano string `json:"ano"`
-		Mes string `json:"mes"`
-	}
-
 	var infos = []info{
 		{UF: "SC", Ano: "25", Mes: "05"},
 		{UF: "SC", Ano: "25", Mes: "04"},
@@ -28,23 +27,54 @@ func main() {
 		fmt.Println(infos[i])
 	}
 
-	resp, err := ftp.Dial("ftp.datasus.gov.br:21", ftp.DialWithTimeout(5*time.Second))
+	path := "ftp.datasus.gov.br:21"
+	login, password := "anonymous", "anonymous"
 
+	resp, err := ConnectFtp(path, login, password)
+	if err != nil {
+		panic(err)
+	}
+
+	ftp_path := "/dissemin/publicos/CNES/200508_/Dados/ST/STSC2506.dbc"
+	download_path := "api/ftp/archives/"
+	name := "teste.dbc"
+	err = ReadAndDownload(resp, download_path, name, ftp_path)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func ConnectFtp(path string, login string, password string) (response *ftp.ServerConn, err error) {
+	resp, err := ftp.Dial(path, ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = resp.Login("anonymous", "anonymous")
+	err = resp.Login(login, password)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	read, err := resp.Retr("/dissemin/publicos/CNES/200508_/Dados/ST/STSC2506.dbc")
+	return resp, nil
+}
+
+func ReadAndDownload(resp *ftp.ServerConn, download_path string, name string, ftp_path string) error {
+	read, err := resp.Retr(ftp_path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	archive, err := os.Create("api/ftp/teste.dbc")
+	_, err = os.Stat(download_path)
+	fmt.Println(err)
+	if err != nil {
+		err = os.MkdirAll(download_path, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	archive, err := os.Create(download_path + name)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -58,4 +88,5 @@ func main() {
 		log.Fatal(err)
 	}
 
+	return nil
 }
