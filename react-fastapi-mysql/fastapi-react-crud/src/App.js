faddab2c2441f486006a93ac51217597e65eff32
brenobmoreira <brenobmoreira20@gmail.com>
2025-12-24 17:18:38 -0300

    create form and table to list

--- react-fastapi-mysql/fastapi-react-crud/src/App.js
+++ react-fastapi-mysql/fastapi-react-crud/src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import api from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    price: 0
+  });
+
+  const fetchItems = async () => {
+    const response = await api.get('/items/');
+    setItems(response.data)
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const {name, type, value} = event.target;
+    const finalValue = type === 'number' ? parseInt(value) || 0: value;
+    setFormData({
+      ...formData,
+      [name]: finalValue,
+    });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await api.post('/items/', formData);
+    console.log('formData', formData);
+    fetchItems();
+    setFormData({
+      name: '',
+      description: '',
+      price: 0
+    });
+  };
+
+    return (
+    <div>
+      {/* Navbar */}
+      <nav className='navbar navbar-dark bg-primary'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='/'>
+            Basic CRUD Operation
+          </a>
+        </div>
+      </nav>
+
+      {/* Form Section */}
+      <div className='container my-5'>
+        <div className='row justify-content-center'>
+          <div className='col-md-6'>
+            <form onSubmit={handleFormSubmit}>
+              <div className='mb-3'>
+                <label htmlFor='name' className='form-label'>
+                  Item Name
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='name'
+                  name='name'
+                  onChange={handleInputChange}
+                  value={formData.name}
+                />
+              </div>
+
+              <div className='mb-3'>
+                <label htmlFor='description' className='form-label'>
+                  Item Description
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='description'
+                  name='description'
+                  onChange={handleInputChange}
+                  value={formData.description}
+                />
+              </div>
+
+              <div className='mb-3'>
+                <label htmlFor='price' className='form-label'>
+                  Item Price
+                </label>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='price'
+                  name='price'
+                  onChange={handleInputChange}
+                  value={formData.price}
+                />
+              </div>
+
+              <button type='submit' className='btn btn-primary w-100'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* Table Section */}
+      <div className='container'>
+        <h2 className='text-center my-4'>Items List</h2>
+        <table className='table table-striped table-bordered'>
+          <thead className='table-dark'>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
